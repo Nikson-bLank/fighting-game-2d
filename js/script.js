8d9443024b6d39007c5d57a813a072071c8e3367
Nikson-blank <niksonjohn11@gmail.com>
2023-07-02 19:37:40 +0530

    added left and right movement for rectangle

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector("canvas");
 
 const c = canvas.getContext("2d");
+const gravity = 0.1;
 
 canvas.width = 1024;
 canvas.height = 576;
@@ -11,16 +12,33 @@ class Sprite {
     constructor({ position, velocity }) {
         this.position = position;
         this.velocity = velocity;
+        this.height = 100;
+        this.width = 50;
     }
 
     draw() {
         c.fillStyle = "red";
-        c.fillRect(this.position.x, this.position.y, 50, 100);
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 
     update() {
         this.draw();
-        this.position.y += 10;
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0;
+        } else {
+            this.velocity.y += gravity;
+        }
+        // if (
+        //     this.position.x + this.width + this.velocity.x >= canvas.width ||
+        //     this.position.x + this.width + this.velocity.x <= 0
+        // ) {
+        //     // console.log("stop");
+        //     this.velocity.x = 0;
+        // } else {
+        //     this.position.x += this.velocity.x;
+        // }
     }
 }
 
@@ -46,17 +64,72 @@ const enemy = new Sprite({
     },
 });
 
+let keys = {
+    a: { pressed: false },
+    d: { pressed: false },
+};
+let lastKeyPressed;
+
 player.draw();
 enemy.draw();
 
 let frame = 0;
+
+const { a, d } = keys;
 function animate() {
-    console.log("frame", frame++);
+    // console.log("frame", frame++);
     window.requestAnimationFrame(animate);
     c.fillStyle = "black";
     c.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
     enemy.update();
+    player.velocity.x = 0;
+    if (a.pressed && lastKeyPressed === "a") {
+        return (player.velocity.x = -2);
+    }
+    if (d.pressed && lastKeyPressed === "d") {
+        return (player.velocity.x = 2);
+    }
 }
 
-// animate();
+animate();
+
+window.addEventListener("keydown", (event) => {
+    let eventKey = event.key;
+    switch (eventKey.toLowerCase()) {
+        case "a":
+            a.pressed = true;
+            lastKeyPressed = "a";
+            break;
+        case "d":
+            d.pressed = true;
+            lastKeyPressed = "d";
+            break;
+
+        default:
+            break;
+    }
+    console.log(
+        "🚀 ~ file: script.js:100 ~ window.addEventListener ~ event.key:",
+        event.key
+    );
+
+    // keys[event.key].pressed = true;
+    // lastKeyPressed = event.key;
+});
+
+window.addEventListener("keyup", (event) => {
+    let eventKey = event.key;
+    switch (eventKey.toLowerCase()) {
+        case "a":
+            a.pressed = false;
+            break;
+        case "d":
+            d.pressed = false;
+            break;
+
+        default:
+            break;
+    }
+    // keys[event.key].pressed = false;
+});
